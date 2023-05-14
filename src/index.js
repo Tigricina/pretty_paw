ad7368c61a1ec7590177ff4dc99eb5702c394911
tigricina <karina6891@mail.ru>
2023-05-14 03:43:18 +0300

    lesson 2. Added style and adaptive except footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import './index.html';
 //new modules
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 import './index.scss';
 
 //use modules 
+const videoBG = document.querySelector('.video-bg');
+// <source src="video/video.webm" type="video/webm">
+//<source src="video/video.mp4" type="video/mp4"></source>
+videoBG.innerHTML = `
+    <source src="video/video.webm" type="video/webm">
+    <source src="video/video.mp4" type="video/mp4"></source>
+`;
 
 
 /*
