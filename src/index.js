4436aacafab72ac1ac14c12ef1ff9439a8a5c819
tigricina <karina6891@mail.ru>
2023-05-15 02:50:02 +0300

    lesson 3. adaptive for about section, add js for slider

--- src/index.js
+++ src/index.js
@@ -3,8 +3,41 @@ import './index.html';
 import 'swiper/scss';
 import 'swiper/scss/pagination';
 import './index.scss';
+import { slidersInit } from './modules/sliders';
 
 //use modules 
+slidersInit('.about__slider', {
+    pagination: {
+        el: '.about__slider-pagination'
+        //bulletClass: 'about__bullet',
+        //bulletActiveClass: 'about__slider-pagination'
+    }
+});
+
+slidersInit('.career__slider', {
+    pagination: {
+        el: '.career__slider-pagination'
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1240: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+      }
+});
+
+
 const videoBG = document.querySelector('.video-bg');
 videoBG.innerHTML = `
     <source src="video/video.webm" type="video/webm">
